85ce0d089a1cd07b7bfb4b65414b576a3046a12f
rchars <rchars@protonmail.com>
2023-03-03 17:06:19 +0100

    first

---
+++ 1/1.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<title>PHP 1</title>
+		<style>
+			body {
+				background-color: black;
+				color: lime;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			echo "<h1>PHP</h1>";
+			echo "<h2>Tha worst</h2>";
+			
+		?>
+	</body>
+</html>
